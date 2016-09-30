ac62a2f0f7289c2c8493ea8b7901fc07ca797753
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-09-30 11:11:05 -0400

    working on sub menu transition

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+var $ = require('jquery');
+
+$('.product-menu-wrapper').hover(function(e){
+  if (e.type === "mouseenter") {
+    $(this).find('.product-menu').addClass('menu-open');
+  }else{
+    $(this).find('.product-menu').removeClass('menu-open');
+  }
+});
