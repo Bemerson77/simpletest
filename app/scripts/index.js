0d19e90eeefc8f5184a83c65cd33405fc8cbe118
Brandon Emerson <Brandon.Emerson77@gmail.com>
2016-09-30 11:27:41 -0400

    sub menu done: completed

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,7 @@
 var $ = require('jquery');
 
+
+// toggles product menu on hover event
 $('.product-menu-wrapper').hover(function(e){
   if (e.type === "mouseenter") {
     $(this).find('.product-menu').addClass('menu-open');
